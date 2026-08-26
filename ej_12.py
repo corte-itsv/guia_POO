bb1724bc38843faaebdf2e5ed98276733b8f8f79
sarce7411 <s.arce940@itsv.edu.ar>
2026-08-26 18:00:35 +0000

    hago ej 12 bine

--- ej_12.py
+++ ej_12.py
@@ -13,3 +13,19 @@ v1.mostrardatos()
 
 v2 = Vehicle("BMW", 200)
 v2.mostrardatos()
+
+class Vehicle:
+    color = 'Red'
+
+    def __init__(self, modelo, velocidad):
+        self.modelo = modelo
+        self.velocidad = velocidad
+
+    def mostrardatos(self):
+        print(f"{self.modelo} - Color: {self.color}, Speed: {self.velocidad}")
+
+v1 = Vehicle("Tesla", 250)
+v1.mostrardatos()
+
+v2 = Vehicle("BMW", 200)
+v2.mostrardatos()
