337440948dda903d2453b7605f2c10f716bf8e4e
Benjamin Maximo Oller Padilla <benjaa@DESKTOP-RTJ1919.localdomain>
2026-08-26 18:59:36 +0000

    Correccion Ejercicio 14

--- ejercicio_14.py
+++ ejercicio_14.py
@@ -10,8 +10,8 @@ class Vehicle:
         print(f"Vehicle: {self.name}, Max Speed: {self.max_speed} km/h")
 
 class Bus(Vehicle):
-    def seating_capacity(self, capacity):
-            super().seating_capacity(capacity)
+    def seating_capacity(self):
+        super().seating_capacity(50)
 
 bus = Bus("School Bus", 120)
-bus.seating_capacity(50)
+bus.seating_capacity()
