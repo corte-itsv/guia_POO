fe7ec699b8ad384e0899bdb2378095c238a561a2
Eitan <e.landenberg543@itsv.edu.ar>
2026-08-26 18:25:37 +0000

    realiza el ejercicio 14

---
+++ ej_14.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    def __init__(self, name, max_speed):
+        self.name=name
+        self.max_speed=max_speed
+    def seating_capacity(self, capacity):
+        print(f"{self.name} seating capacity is: {capacity}")
+
+class Bus(Vehicle):
+    def seating_capacity(self):
+        super().seating_capacity(50)
+
+bus1 = Bus("School Bus", 120)
+bus1.seating_capacity()
