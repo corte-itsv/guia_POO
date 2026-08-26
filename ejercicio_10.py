64a0bc789e65dc72c4cfcc4a0b9ea0193fd964a1
Felipe Vivas <f.vivas.lucero547@itsv.edu.ar>
2026-08-26 16:22:40 +0000

    ejercicio 10

---
+++ ejercicio_10.py
@@ -0,0 +1,19 @@
+class Notebook:
+    def __init__(self):
+        self.notes = []
+
+    def add_note(self, note):
+        self.notes.append(note)
+
+    def show_notes(self):
+        for number, note in enumerate(self.notes, start=1):
+            print(f"{number}. {note}")
+
+
+notebook = Notebook()
+
+notebook.add_note("Buy groceries")
+notebook.add_note("Read a book")
+notebook.add_note("Call the doctor")
+
+notebook.show_notes()
