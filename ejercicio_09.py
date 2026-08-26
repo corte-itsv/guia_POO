ab8052294c4f8492dcbba2b52acebef48b4612c1
Benjamin Maximo Oller Padilla <benjaa@DESKTOP-RTJ1919.localdomain>
2026-08-26 18:26:35 +0000

    Correccion ejercicio 9

--- ejercicio_09.py
+++ ejercicio_09.py
@@ -4,13 +4,13 @@ class Temperature:
 
     def to_fahrenheit(self):
         fahrenheit = (self.celsius * 1.8) + 32
-        print(f"Fahrenheit: {fahrenheit}")
+        return fahrenheit
 
     def to_kelvin(self):
         kelvin = self.celsius + 273.15
-        print(f"Kelvin: {kelvin}")
+        return kelvin
 
 t = Temperature(100)
 print(f"Celsius: {t.celsius}")
-t.to_fahrenheit()
-t.to_kelvin()
+print(f"Fahrenheit: {t.to_fahrenheit()}")
+print(f"Kelvin: {t.to_kelvin()}")
