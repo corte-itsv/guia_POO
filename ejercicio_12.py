1d7dea2e08476a5ed86013dddefd5eafa6bfbcf9
Felipe Vivas <f.vivas.lucero547@itsv.edu.ar>
2026-08-26 18:33:17 +0000

    ejercicio 12

---
+++ ejercicio_12.py
@@ -0,0 +1,24 @@
+# Ejercicio 12: Atributo de clase compartido entre instancias
+
+class Vehicle:
+    color = "White"
+
+    def __init__(self, name, max_speed):
+        self.name = name
+        self.max_speed = max_speed
+
+    def show_info(self):
+        print(f"{self.name} - Color: {Vehicle.color}, Speed: {self.max_speed}")
+
+
+
+v1 = Vehicle("Tesla", 250)
+v2 = Vehicle("BMW", 200)
+
+v1.show_info()
+v2.show_info()
+
+Vehicle.color = "Red"
+
+v1.show_info()
+v2.show_info()
