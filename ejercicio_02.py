6c1c08b93314e04e0ce6d3d2d72cad61de136789
Lucas Andino <l.andino568@itsv.edu.ar>
2026-08-26 18:56:35 +0000

    es el ejercicio 02 pero corregido de milage a mileage

--- ejercicio_02.py
+++ ejercicio_02.py
@@ -2,6 +2,6 @@ class Vehicle:
     def __init__(self, name, max_speed, mileage):
         self.name = name
         self.max_speed = max_speed
-        self.milage = mileage
+        self.mileage = mileage
 vehicle1 = Vehicle("Tesla Model S", 250, 18)
-print(f"Vehicle Name: {vehicle1.name},Speed: {vehicle1.max_speed}, Mileage: {vehicle1.milage}")
+print(f"Vehicle Name: {vehicle1.name}, Speed: {vehicle1.max_speed}, Mileage: {vehicle1.mileage}")
