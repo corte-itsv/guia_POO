a958a4269a601b6aa71ad569db3f806d8cc2059c
Octavio Gambino <o.gambino250@itsv.edu.ar>
2026-09-09 17:30:52 +0000

    ej 2 no terminado

--- ejercicio_02.py
+++ ejercicio_02.py
@@ -1,2 +1,8 @@
 class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
     
+vehicle1 = Vehicle("Tesla Model S", 250, 18)
+print()
