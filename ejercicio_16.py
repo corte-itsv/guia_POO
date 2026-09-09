1b276f74533db3cb1d10c4051ad28fc60d8ab5f4
Jeremias Purro <j.purro119@itsv.edu.ar>
2026-09-09 18:50:53 +0000

    Resuelve ejercicio 16

---
+++ ejercicio_16.py
@@ -0,0 +1,18 @@
+class Animal:
+    def speak(self):
+        return ""
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow"
+
+
+dog = Dog()
+cat = Cat()
+
+print(f"Dog says: {dog.speak()}!")
+print(f"Cat says: {cat.speak()}!")
