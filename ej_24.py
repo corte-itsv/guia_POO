43e6d561ae8810b9fd78f5faddda14c2b194f45c
sarce7411 <s.arce940@itsv.edu.ar>
2026-09-16 17:30:37 +0000

    hago ej 24

---
+++ ej_24.py
@@ -0,0 +1,17 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+    def __add__(self, otro):
+        
+        return Vector(self.x + otro.x, self.y + otro.y)
+    def __repr__(self):
+        return f"Vector({self.x}, {self.y})"
+
+
+v1 = Vector(2, 3)
+v2 = Vector(4, 1)
+r = v1 + v2
+print(r)
