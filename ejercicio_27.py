e5a24ee57771e75d4ebb38b4533202745a44c6d4
Lucca Garcia <l.garcia.269@itsv.edu.ar>
2026-09-23 16:22:04 +0000

    Agrega ejercicio 27

---
+++ ejercicio_27.py
@@ -0,0 +1,43 @@
+class Animal:
+    def eat(self):
+        return "eating."
+
+
+
+class Lion(Animal):
+    def eat(self):
+        return "Lion eats meat."
+
+
+
+class Elephant(Animal):
+    def eat(self):
+        return "Elephant eats grass."
+
+
+
+class Parrot(Animal):
+    def eat(self):
+        return "Parrot eats seeds."
+
+
+
+
+class Zoo:
+    def __init__(self):
+        self.animals = []
+
+    def add_animal(self, animal):
+        self.animals.append(animal)
+
+    def feed_all(self):
+        for animal in self.animals:
+            print(animal.eat())
+
+
+
+zoo = Zoo()
+zoo.add_animal(Lion())
+zoo.add_animal(Elephant())
+zoo.add_animal(Parrot())
+zoo.feed_all()
